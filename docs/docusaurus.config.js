d049258cc97882477adc63e2c5f4fe5e3df62833
Luigi Denora <ldenora@codearchitects.com>
2023-07-16 14:17:53 +0000

    update icon

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
         title: 'My Site',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo/android-chrome-512x512.png',
         },
         items: [
           {
