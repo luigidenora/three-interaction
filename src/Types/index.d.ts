3ed3b2acacd3e22098999922bfe812826746b078
Andrea Gargaro <andreagargaro_ca@hotmail.com>
2023-08-03 13:05:38 +0200

    Types

--- src/Types/index.d.ts
+++ src/Types/index.d.ts
@@ -1,10 +1,16 @@
 export * from "./Camera";
+export * from "./CubeCamera";
+export * from "./Group";
 export * from "./InstancedMesh";
+export * from "./Line";
 export * from "./Mesh";
 export * from "./Object3D";
 export * from "./OrthographicCamera";
 export * from "./PerspectiveCamera";
+export * from "./Points";
 export * from "./Scene";
+export * from "./SkinnedMesh";
+export * from "./Sprite";
 
 // @ts-ignore
 export * from 'three/index';
